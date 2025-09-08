8a7c3cc9bae6561ce8798f48f3760c50ba6b2093
sut-fri <srf8625@nyu.edu>
2025-09-08 18:58:22 -0400

    2

---
+++ recipe/src/index.js
@@ -0,0 +1,13 @@
+import './styles.css'
+import KITTY_IMG from '../src/assets/kitty.jpg'
+//import react librerary core
+import React from 'react'
+//react dom allows us to write JSX for web browsers
+import ReactDOM from 'react-dom/client'
+//to use componet: import it first
+import App from './App'
+//optional: import gloable CSS file
+import './global.css'
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<App />)
